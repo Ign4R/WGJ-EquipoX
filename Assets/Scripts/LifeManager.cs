dd726168fb53c417f2527864a74f9655bbec8691
ziinovv <augustocouture44@gmail.com>
2024-08-11 14:57:51 -0300

    Fix- Timer state

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -20,7 +20,7 @@ public class LifeManager : MonoBehaviour
         }
     }
 
-    private void LoseLife()
+    public void LoseLife()
     {
         if (lives > 0)
         {
--- Assets/Scripts/TimerState.cs
+++ Assets/Scripts/TimerState.cs
@@ -1,21 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimerState : MonoBehaviour
 {
-     public enum GameState { Estado1, Estado2}
-     public GameState currentState;
+    public enum GameState { State1, State2 }
+    public GameState currentState;
 
     [SerializeField] private Slider timeSlider;
-    [SerializeField] private float maxTime = 60f;
+    [SerializeField] private float maxTimeState1 = 60f;
+    [SerializeField] private float maxTimeState2 = 60f;
     [SerializeField] private float waitTimeBetweenStates = 2f;
+
+    [SerializeField] private LifeManager lifeManager;  
+    [SerializeField] private Color estado1Color = Color.green;
+    [SerializeField] private Color estado2Color = Color.red;
 
     private void Start()
     {
-        currentState = GameState.Estado1;
-        timeSlider.maxValue = maxTime;
+        currentState = GameState.State1;
+        timeSlider.maxValue = maxTimeState1;
+        timeSlider.fillRect.GetComponent<Image>().color = estado1Color; 
         StartCoroutine(StateLoop());
     }
 
@@ -25,32 +30,34 @@ public class TimerState : MonoBehaviour
         {
             switch (currentState)
             {
-                case GameState.Estado1:
+                case GameState.State1:
                     Debug.Log("Estado Normal");
-                    yield return StartCoroutine(StateTimer(maxTime));
+                    yield return StartCoroutine(StateTimer(maxTimeState1, 1f));
                     yield return new WaitForSeconds(waitTimeBetweenStates);
-                    currentState = GameState.Estado2;
+                    currentState = GameState.State2;
+                    timeSlider.fillRect.GetComponent<Image>().color = estado2Color;  
                     break;
 
-                case GameState.Estado2:
-                    yield return StartCoroutine(StateTimer(maxTime));
-                    yield return new WaitForSeconds(waitTimeBetweenStates);
+                case GameState.State2:
                     Debug.Log("Estado Fantasma");
-                    yield break;
+                    yield return StartCoroutine(StateTimer(maxTimeState2, 1f)); 
+                    yield return new WaitForSeconds(waitTimeBetweenStates);
+                    currentState = GameState.State1;
+                    timeSlider.fillRect.GetComponent<Image>().color = estado1Color;  
+                    lifeManager.LoseLife();  
+                    break;
             }
         }
     }
 
-    private IEnumerator StateTimer(float time)
+    private IEnumerator StateTimer(float time, float timeFactor)
     {
         while (time > 0)
         {
             timeSlider.value = time;
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(1f * timeFactor); 
             time -= 1f;
         }
         timeSlider.value = 0;
     }
 }
-
-
