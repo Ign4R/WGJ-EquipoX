129e6239f957254c182e8d7339325daa05f47175
Emanuel Jara <40744060+fruei@users.noreply.github.com>
2024-08-10 21:48:10 -0300

    AudioManager
    agregar audio manager y clase de sonido para manejar el volumen, reproduccion, pausa, muteo y volumen de las pistas por titulo

---
+++ Assets/Scripts/SoundManager/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager main;
+
+    void Awake()
+    {
+        if (main == null)
+            main = this;
+        else
+            Destroy(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+
+        Play("CancionPrincipal");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            return;
+
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            return;
+
+        s.source.Stop();
+    }
+
+    public void Mute(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            return;
+        s.source.mute = !s.source.mute;
+    }
+
+    public void Volume(string name, float volume)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            return;
+        s.source.volume = volume;
+    }
+}
---
+++ Assets/Scripts/SoundManager/TestSoundManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TestSoundManager : MonoBehaviour
+{
+   public string soundName;
+   [Range(0, 1)]
+   public float volume;
+}
+
+
+[CustomEditor(typeof(TestSoundManager))]
+public class OpenFileButtonScript : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        TestSoundManager testSoundManager = (TestSoundManager)target;
+        testSoundManager.soundName = EditorGUILayout.TextField("SoundName", testSoundManager.soundName);
+        testSoundManager.volume = EditorGUILayout.Slider("Volume",testSoundManager.volume,0,1);
+        if(GUILayout.Button("PlaySound"))
+        {
+            AudioManager.main.Play(testSoundManager.soundName);
+        }
+        if(GUILayout.Button("StopSound"))
+        {
+            AudioManager.main.Stop(testSoundManager.soundName);
+        }
+        if(GUILayout.Button("MuteSound"))
+        {
+            AudioManager.main.Mute(testSoundManager.soundName);
+        }
+        if(GUILayout.Button("SetVolume"))
+        {
+            AudioManager.main.Volume(testSoundManager.soundName, testSoundManager.volume);
+        }
+    }
+  
+}
