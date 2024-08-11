5b12d3e4b6324c7622fcecee5f791e4cf404ebfb
ziinovv <augustocouture44@gmail.com>
2024-08-11 12:21:34 -0300

    lifemanager

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LifeManager : MonoBehaviour
+{
+    public int lives = 3;
+    public Image[] lifeImages;
+
+    private void Start()
+    {
+        UpdateLivesUI();  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            LoseLife();  
+        }
+    }
+
+    private void LoseLife()
+    {
+        if (lives > 0)
+        {
+            lives--;  
+            UpdateLivesUI(); 
+
+            if (lives <= 0)
+            {
+                GameOver();  
+            }
+        }
+    }
+
+    private void UpdateLivesUI()
+    {      
+        for (int i = 0; i < lifeImages.Length; i++)
+        {
+            if (i < lives)
+            {
+                lifeImages[i].enabled = true; 
+            }
+            else
+            {
+                lifeImages[i].enabled = false;  
+            }
+        }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+}
+
+
